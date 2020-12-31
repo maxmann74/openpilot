7a8e4805eab2fbb0ce7b5e16df14e20aeb802d5b
cfranhonda <46506059+cfranhonda@users.noreply.github.com>
2020-12-31 14:24:01 -0800

    give this a real shot, drive for a day.

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -448,7 +448,7 @@ class CarInterface(CarInterfaceBase):
 
     if candidate in HONDA_BOSCH:
       ret.gasMaxBP = [0.0, 5., 10., 22., 35.] # m/s
-      ret.gasMaxV = [0.36, 0.24, 0.18, 0.16, 0.12]
+      ret.gasMaxV = [0.36, 0.24, 0.19, 0.17, 0.16]
       ret.brakeMaxBP = [3., 5., 22.]  # m/s
       ret.brakeMaxV = [1., 0.8, 0.7]   # max brake allowed
     else:
